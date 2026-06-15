990a4082a1728b838ff051e68cf3d07681cfb04f
zhco <luycheung@qq.com>
2026-06-15 11:58:24 +0800

    v2.0.0.24: bump version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.termuxbuilder"
         minSdk = 26
         targetSdk = 34
-        versionCode = 23
-        versionName = "2.0.0.23"
+        versionCode = 24
+        versionName = "2.0.0.24"
     }
 
     buildTypes {
